08a584edca59cd84bb8873b1e76b4411bc5f1e65
tediorelee <tediorelee@leejieun.fan>
2024-06-29 00:19:27 +0800

    feat: init

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,71 @@
-import { Context, Schema } from 'koishi'
+import { Context, Schema, h } from 'koishi'
 import { resolve } from 'path'
 import {} from '@koishijs/plugin-console'
 
 export const name = 'cobalt'
 
-export interface Config {}
+export interface Config {
+  api: string;
+}
 
-export const Config: Schema<Config> = Schema.object({})
+export const Config = Schema.object({
+  api: Schema.string().default('https://api.cobalt.tools/api/json').required().description('填写cobalt的api地址, 需要接上/api/json'),
+  description: Schema.string().default('自行部署教程地址').description('https://github.com/imputnet/cobalt/blob/current/docs/run-an-instance.md'),
+})
 
-export function apply(ctx: Context) {
+
+export function apply(ctx: Context, config: Config) {
+  async function fetchFromAPI(url: string) {
+    const headers = {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    };
+    const params = {
+      url,
+      vQuality: "1080" // set video quality default 1080p(if possible)
+    };
+    return await ctx.http.post(config.api, params, { headers });
+  };
+
+  ctx.command('cobalt <link:text>', 'Download with cobalt')
+    .action(async ({session}, link) => {
+      try {
+        if (!link) return '缺少url链接!'
+        const result = await fetchFromAPI(link)
+        const {
+          status, // error / redirect / stream / success / rate-limit / picker
+          text,
+          url,
+          // pickerType,
+          // picker,
+          // audio
+        } = result;
+
+        switch (status) {
+          case 'error':
+            session.send(`下载错误: ${text}`);
+            break;
+          case 'redirect':
+            session.send(h.video(url));
+            break;
+          case 'stream':
+            session.send(h.video(url));
+            break;
+        }
+        
+        
+      } catch(err) {
+        const {
+          data: {
+            status,
+            text
+          },
+          status: code
+        } = err?.response
+        return `发生错误${code}! ${text}`;
+      }
+    });
+
   ctx.inject(['console'], (ctx) => {
     ctx.console.addEntry({
       dev: resolve(__dirname, '../client/index.ts'),
